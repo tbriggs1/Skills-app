a4f39d828ae26b23b04414df0741b87a15a350ea
tbriggs1 <tombriggs93@protonmail.com>
2021-04-03 06:28:51 +0100

    Added custom methods

--- skillsapi/views.py
+++ skillsapi/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
 from .models import Skill, SubSkill
 from .serializers import SkillSerializer, SubSkillSerializer
 
@@ -8,6 +10,14 @@ class SkillsViewSet(viewsets.ModelViewSet):
     queryset = Skill.objects.all()
     serializer_class = SkillSerializer
 
+    @action(detail=True, methods=['POST'])
+    def rate_skill(self, request, pk=None):
+        response = {'message': 'its working'}
+        return Response(response, status=status.HTTP_200_OK)
+
 class SubSkillsViewSet(viewsets.ModelViewSet):
     queryset = SubSkill.objects.all()
     serializer_class = SubSkillSerializer
+
+
+
