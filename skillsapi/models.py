1d793dd9dc5204203c37a5628c86ca9975fec85b
tbriggs1 <tombriggs93@protonmail.com>
2021-04-05 19:14:53 +0100

    Added different field to DB

--- skillsapi/models.py
+++ skillsapi/models.py
@@ -3,8 +3,11 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Skill(models.Model):
-    title = models.CharField(max_length=32)
-    description = models.CharField(max_length=360)
+    title = models.CharField(max_length=32, default="Add Skill")
+    subSkill = models.CharField(max_length=32, default="Add Second Skill")
+    employee_rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
+    manager_rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], default=0)
+    studID = models.CharField(max_length=32, default="default_user")
 
 class SubSkill(models.Model):
     title = models.CharField(max_length=32)
--- skillsapi/serializers.py
+++ skillsapi/serializers.py
@@ -4,9 +4,11 @@ from .models import Skill, SubSkill
 class SkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = Skill
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'subSkill', 'employee_rating', 'manager_rating', 'studID')
 
 class SubSkillSerializer(serializers.ModelSerializer):
     class Meta:
         model = SubSkill
         fields = ('id', 'title', 'description', 'skills', 'stars')
+
+
